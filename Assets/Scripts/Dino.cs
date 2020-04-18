4c165a7769c3f9dd6119fd8ad1610fbaf1770f3a
Adrieldf <adriel.idf@gmail.com>
2020-04-18 15:42:56 -0300

    Base project, some sprites and barely some movement going

---
+++ Assets/Scripts/Dino.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dino : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody2D rb = null;
+    [SerializeField]
+    private float speed = 10f;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        Vector2 vel = new Vector2(speed, 0);
+
+        rb.velocity = vel;
+    }
+}
