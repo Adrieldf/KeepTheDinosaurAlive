dbf2d6124044d3ec1c569d391b0e54b4e5ae9ce0
Adrieldf <adriel.idf@gmail.com>
2020-04-18 23:38:26 -0300

    player movement, infinite level, ui sprites and score

--- Assets/Scripts/Dino.cs
+++ Assets/Scripts/Dino.cs
@@ -8,15 +8,37 @@ public class Dino : MonoBehaviour
     private Rigidbody2D rb = null;
     [SerializeField]
     private float speed = 10f;
-    void Start()
-    {
+    [SerializeField]
+    private float jumpForce = 100f;
+    private bool isGrounded = false;
+    private float extendedJumpTimeLeft = 0f;
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+            isGrounded = true;
     }
-
-    void Update()
+    private void OnCollisionExit2D(Collision2D collision)
     {
-        Vector2 vel = new Vector2(speed, 0);
+        if (collision.gameObject.CompareTag("Ground"))
+            isGrounded = false;
+    }
+    void FixedUpdate()
+    {
+        Vector2 vel = new Vector2(speed, rb.velocity.y);
+
+        if (extendedJumpTimeLeft > 0f)
+            extendedJumpTimeLeft -= Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.Space) && (isGrounded || extendedJumpTimeLeft > 0f))
+        {
+            rb.AddForce(new Vector2(0, jumpForce));
+            if (isGrounded)
+                extendedJumpTimeLeft = 0.14f;
+        }
 
         rb.velocity = vel;
+
     }
+
 }
---
+++ Assets/Scripts/Master.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Master : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject player = null;
+    private float nextPosition = 450f;
+    private float posToInsert = 490f;
+    private float increaseAmount = 500f;
+    [SerializeField]
+    private GameObject groundParent = null;
+    [SerializeField]
+    private GameObject ground = null;
+    [SerializeField]
+    private GameObject skyParent = null;
+    [SerializeField]
+    private GameObject sky = null;
+    [SerializeField]
+    private TextMeshProUGUI score = null;
+
+
+    private void FixedUpdate()
+    {
+        if (player.transform.position.x > nextPosition)
+        {
+            nextPosition += increaseAmount;
+            Instantiate(sky, new Vector3(posToInsert, 0f, 0f), Quaternion.identity, skyParent.transform);
+            Instantiate(ground, new Vector3(posToInsert, 0.02f, 0f), Quaternion.identity, groundParent.transform);
+            posToInsert += increaseAmount;
+        }
+    }
+    private void Update()
+    {
+        score.text = player.transform.position.x.ToString("N0");
+    }
+}
